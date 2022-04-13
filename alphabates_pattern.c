1bc786023d9e4ec90812c9006feabef553c30617
RAJA S J THAKUR <91691777+RAJA3110@users.noreply.github.com>
2022-04-13 14:33:29 +0530

    Add files via upload

---
+++ alphabates_pattern.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int i , j  ;
+    int n , m ;
+    int a='A';
+    printf("enter the pattern detail such as row\n");
+    scanf("%d", &m );  
+    printf("enter the pattern detail such as colunm\n");
+    scanf("%d", &n );
+    for (i=1;i<=m;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                printf("%c ",a);
+                a=a+1;
+            }
+           printf("\n");     
+        }
+    return 0;
+
+}
